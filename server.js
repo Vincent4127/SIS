61072d583cc3838474d339294844bcc88960cbf8
Vincent <vincent.g.raya@gmail.com>
2025-11-08 14:43:54 +0200

    html read

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-app.use(express.static(path.join(__dirname, "../public")));
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static('.'));
 
 await connectDB();
 const db = getDB();
